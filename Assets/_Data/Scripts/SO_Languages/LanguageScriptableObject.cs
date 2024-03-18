13e0c6f4f0b91a3cba7bf6d9181dafcec0b3fd39
AleksArtemev <asdg.asdg.99@bk.ru>
2024-03-18 21:21:05 +0300

    new update

--- Assets/_Data/Scripts/SO_Languages/LanguageScriptableObject.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "create language", menuName = "New Languages")]
-public class LanguageScriptableObject : ScriptableObject
-{
-    [SerializeField] private GameObject[] Language;
-}
